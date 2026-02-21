41dde9b4b59456cb04e58191c2aa51a70f9b4563
StrangeQuark <zacharyzirkle@hotmail.com>
2026-02-20 18:35:50 -0600

    Init spring boot application, use pom from msinit services

---
+++ src/main/java/com/strangequark/trasck/TrasckApplication.java
@@ -0,0 +1,13 @@
+package com.strangequark.trasck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrasckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrasckApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/strangequark/trasck/TrasckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.strangequark.trasck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrasckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
